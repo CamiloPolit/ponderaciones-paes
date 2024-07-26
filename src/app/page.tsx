b137faefc60c4a7f7919d0b832e4bd6d1c86d167
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-07-26 05:33:10 -0400

    feat: Al presionar tab se autocompleta el input de selección de universidad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { MdBlock } from "react-icons/md";
 import { IconContext } from "react-icons";
 
@@ -13,20 +13,31 @@ export default function Page() {
 
   const [inputValue, setInputValue] = useState("");
   const [highlightedText, setHighlightedText] = useState("");
+  const [matchedText, setMatchedText] = useState("");
+  const maskRef = useRef();
 
   const handleChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
 
-    let match = suggestions.find((suggestion) =>
+    const match = suggestions.find((suggestion) =>
       suggestion.toLowerCase().startsWith(value.toLowerCase()),
     );
+    setMatchedText(match);
 
     console.log(match);
     if (match && value) {
-      match = value.slice(0).concat(match.slice(value.length));
-      setHighlightedText(match);
+      const matchModified = value.slice(0).concat(match.slice(value.length));
+      setHighlightedText(matchModified);
     } else {
+      setHighlightedText("");
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Tab" && highlightedText) {
+      e.preventDefault();
+      setInputValue(matchedText);
       setHighlightedText("");
     }
   };
@@ -35,15 +46,21 @@ export default function Page() {
 
   return (
     <div className="flex">
-      {/* <img className="h-min" src="/favicons/favicon-32x32.png" alt="Logo UCh" /> */}
-      <MdBlock size="32px" className="text-gray-500" />
+      <img className="h-min" src="/favicons/favicon-32x32.png" alt="Logo UCh" />
+      {/* <MdBlock size="32px" className="text-gray-500" /> */}
       <div className="h-full" style={{ position: "relative" }}>
-        <input type="text" value={inputValue} onChange={handleChange} />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
         {highlightedText && inputValue && (
           <input
             type="text"
             value={highlightedText}
             readOnly
+            ref={maskRef}
             className="pointer-events-none absolute left-0 top-0 border-transparent bg-transparent text-black/50"
           />
         )}

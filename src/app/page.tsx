0d1a89d093fc5690dd76e4cf26bc55c5f49bcba5
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-07-26 17:39:32 -0400

    refactor: Se añadió una carpeta components agregando el componente del buscador de universidades, e insertándolo en la página de next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,74 +1,10 @@
-"use client";
-import React, { useState, useRef } from "react";
-import { MdBlock } from "react-icons/md";
-import universities from "./universities.json";
+import React from "react";
+import UniversitySearch from "../components/UniversitySearch";
 
-export default function Page() {
-  const suggestions = universities.Universities;
-  const [inputValue, setInputValue] = useState("");
-  const [highlightedText, setHighlightedText] = useState("");
-  const [matchedText, setMatchedText] = useState("");
-  const [selectedUniversity, setSelectedUniversity] = useState(false);
-
-  const handleChange = (e) => {
-    setSelectedUniversity(false);
-    const value = e.target.value;
-    setInputValue(value);
-    const match = suggestions.find((suggestion) =>
-      suggestion.name.toLowerCase().startsWith(value.toLowerCase()),
-    );
-    setMatchedText(match);
-
-    if (match && value) {
-      const matchModified = value
-        .slice(0)
-        .concat(match.name.slice(value.length));
-      setHighlightedText(matchModified);
-    } else {
-      setHighlightedText("");
-    }
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Tab" && highlightedText) {
-      e.preventDefault();
-      setInputValue(matchedText.name);
-      setHighlightedText("");
-      setSelectedUniversity(true);
-    }
-  };
-
-  console.log(highlightedText);
-
+export default function Home() {
   return (
-    <div className="flex">
-      {(highlightedText || selectedUniversity) && (
-        <img
-          className="h-min"
-          src="/favicons/favicon-32x32.png"
-          alt="Logo UCh"
-        />
-      )}
-      {!highlightedText && !selectedUniversity && (
-        <MdBlock size="32px" className="text-gray-500" />
-      )}
-      <div className="h-full" style={{ position: "relative" }}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Ejemplo: Universidad de Chile"
-        />
-        {inputValue && (
-          <input
-            type="text"
-            value={highlightedText}
-            readOnly
-            className="pointer-events-none absolute left-0 top-0 border-transparent bg-transparent text-black/50"
-          />
-        )}
-      </div>
-    </div>
+    <>
+      <UniversitySearch />
+    </>
   );
 }

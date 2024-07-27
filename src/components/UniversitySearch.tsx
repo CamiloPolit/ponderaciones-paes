14b0079d239dfaadce1545d4e74b23f48ec68826
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-07-26 21:43:48 -0400

    fix: Se solucionó un bug relacionados con los nombres del src de la imagen

--- src/components/UniversitySearch.tsx
+++ src/components/UniversitySearch.tsx
@@ -24,7 +24,7 @@ export default function Page() {
     );
     setImageSrc(
       match
-        ? "/favicons/".concat(
+        ? "/logos/".concat(
             match.abbreviation?.toLocaleLowerCase().concat(".png"),
           )
         : "",

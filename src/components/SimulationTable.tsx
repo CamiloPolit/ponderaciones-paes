a2b036ba6852b3a6192e13fad469888d56a5b393
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-07-28 02:16:34 -0400

    feat: Se limitó como máximo el tipear sólo 4 carácteres en los input de la tabla de ponderación

--- src/components/SimulationTable.tsx
+++ src/components/SimulationTable.tsx
@@ -1,16 +1,16 @@
 export default function SimulationTable({ labels }) {
   return (
     <div className="grid grid-cols-2">
-      {labels.map((label) =>
+      {labels.map((label, index) =>
         label === "Send" ? (
           <div
             key={label}
             className="flex flex-col items-center justify-center p-2"
           >
             <p className="cursor-default opacity-0">This have opacity 0</p>
-            <div className="flex h-12 w-20 cursor-pointer items-center justify-center rounded-3xl border-2 bg-slate-600 text-slate-200 hover:bg-slate-700">
+            <button className="flex h-12 w-20 cursor-pointer items-center justify-center rounded-3xl border-2 bg-slate-600 text-slate-100 hover:bg-slate-700">
               <p>Calcular</p>
-            </div>
+            </button>
           </div>
         ) : (
           <div
@@ -21,6 +21,7 @@ export default function SimulationTable({ labels }) {
             <input
               type="text"
               className="w-20 rounded-3xl border-2 p-2 text-center text-xl text-black/85 hover:border-black"
+              maxLength="4"
             />
           </div>
         ),

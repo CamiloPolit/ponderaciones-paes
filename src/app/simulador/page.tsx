3981bf6f0c4223adeb394e3bc9614a7c529f263b
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-08-11 14:45:35 -0400

    fix: Se eliminaron atributos pertenecientes a motion, en un div nativo

--- src/app/simulador/page.tsx
+++ src/app/simulador/page.tsx
@@ -86,12 +86,7 @@ export default function Simulador() {
       className="flex h-[87vh] flex-col items-center justify-center"
     >
       <div className="w-11/12">
-        <div
-          initial={{ opacity: 0, y: 300 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
-          className="m-auto w-full gap-10 rounded-xl border-[1px] border-gray-300 py-5 md:flex md:w-2/3 md:items-center md:justify-center"
-        >
+        <div className="m-auto w-full gap-10 rounded-xl border-[1px] border-gray-300 py-5 md:flex md:w-2/3 md:items-center md:justify-center">
           <div className="flex flex-col items-center justify-center">
             <div className="xs:px-7">
               <UniversitySearch

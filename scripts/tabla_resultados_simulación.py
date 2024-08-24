f70662364e3bb946c3a3d4e69c2ca1e307815152
Camilo Huerta <camilo.alonso.huerta@gmail.com>
2024-08-23 20:25:37 -0400

    script: Se modificaron los archivos dependientes de puntaje_corte_mujeres.py para que puedan funcionar correctamente

--- scripts/tabla_resultados_simulación.py
+++ scripts/tabla_resultados_simulación.py
@@ -4,7 +4,7 @@ fetch_table = pd.read_csv('/content/drive/MyDrive/bbdd/TABLA_FETCH_CARRERAS.csv'
 requirements_table = pd.read_csv('/content/drive/MyDrive/bbdd/REQUISITOS_ADMISION_CARRERAS.csv')
 program_duration_value_table = pd.read_csv('/content/drive/MyDrive/bbdd/TABLA_VALORES_DURACION_CARRERAS.csv')
 correct_answers = pd.read_csv('/content/drive/MyDrive/bbdd/PREGUNTAS_CORRECTAS_CARRERAS.csv')
-women_percentage = pd.read_csv('/content/drive/MyDrive/bbdd/PORCENTAJE_MUJERES_CARRERAS.csv')
+special_admission = pd.read_csv('/content/drive/MyDrive/bbdd/PUNTAJE_CORTE_ADM_ESPECIAL.csv')
 scores_metrics = pd.read_csv('/content/drive/MyDrive/bbdd/ESTADISTICAS_PUNTAJES_CARRERAS.csv')
 grades_table = pd.read_csv('/content/drive/MyDrive/bbdd/INFORMACION_ALUMNOS_PROMEDIOS_POR_CARRERA.csv')
 universities_location = pd.read_csv('/content/drive/MyDrive/bbdd/TABLA_SEDES_UNIVERSIDADES.csv')
@@ -22,7 +22,7 @@ merged_df = pd.merge(merged_df, scores_metrics, on='CODIGO_CARRERA', how='inner'
 merged_df = pd.merge(merged_df, correct_answers, on='CODIGO_CARRERA', how='inner')
 merged_df = pd.merge(merged_df, max_scores, on='CODIGO_CARRERA', how='left')
 merged_df = pd.merge(merged_df, requirements_table, left_on='CODIGO_CARRERA', right_on='COD_CARRERA', how='inner')
-merged_df = pd.merge(merged_df, women_percentage, left_on='CODIGO_CARRERA', right_on='codigo_demre', how='inner')
+merged_df = pd.merge(merged_df, special_admission, left_on='CODIGO_CARRERA', right_on='codigo_demre', how='inner')
 
 # Realiza el merge con last_woman_admitted_score, rellenando con ceros si no hay coincidencias
 merged_df = pd.merge(merged_df, last_woman_admitted_score, on='CODIGO_CARRERA', how='left')
